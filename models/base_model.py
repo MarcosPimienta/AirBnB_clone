b137731461d673bd172b83d961c35147984d8606
MarcosPimienta <1676@holbertonschool.com>
2020-10-30 18:42:31 +0000

    save import __init__

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 
 import uuid
 from datetime import datetime
+from models.__init__ import storage
 
 
 class BaseModel:
@@ -29,6 +30,7 @@ class BaseModel:
                         self.id = str(uuid.uuid4())
                         self.created_at = datetime.now()
                         self.updated_at = datetime.now()
+                        storage.new(self)
 
         def __str__(self):
                 """ Return a printed readable string """
@@ -41,6 +43,7 @@ class BaseModel:
                         updatedat with the current time """
 
                 self.updated_at = datetime.now()
+                storage.save()
 
         def to_dict(self):
                 """returns a dictionary containing all keys/values of
