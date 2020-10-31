3cb6c8d232ef61aa7ac920f29980efc891e05335
Ana-Morales <moralesanaruth@gmail.com>
2020-10-31 16:18:01 +0000

    Add create method

--- console.py
+++ console.py
@@ -2,6 +2,8 @@
 """ Contains the entry point of the command interpreter """
 
 import cmd
+import models
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -20,6 +22,22 @@ class HBNBCommand(cmd.Cmd):
         def emptyline(self):
                 """prints prompt again"""
                 pass
+
+        def do_create(self, arg):
+                """Creates a new instance of BaseModel,
+                saves it (to the JSON file) and prints the id"""
+                args = arg.split()
+                d = {"BaseModel": BaseModel}
+                if len(args) == 0:
+                        print("** class name missing **")
+                        return False
+                if args[0] not in d:
+                        print("** class doesn't exist **")
+                        return False
+                else:
+                        ins = d[args[0]]()
+                        ins.save()
+                        print(ins.id)
 
 if __name__ == '__main__':
         HBNBCommand().cmdloop()
