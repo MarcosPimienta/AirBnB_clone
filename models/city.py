d94c919124195b26f123070547af0887a9842f3e
Ana-Morales <moralesanaruth@gmail.com>
2020-11-01 16:31:50 +0000

    Add new subclasses

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Class City that inherits from BaseModel """
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+        """City Class """
+        state_id = ""
+        name = ""
