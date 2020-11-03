2d77b415a3eae54e2e15b9b0b89d10665d31fbea
Ana-Morales <moralesanaruth@gmail.com>
2020-11-03 20:08:31 +0000

    fix pep8

--- tests/test_console.py
+++ tests/test_console.py
@@ -18,6 +18,7 @@ from models.review import Review
 from models.engine.file_storage import FileStorage
 from unittest.mock import patch
 
+
 class TestConsole(unittest.TestCase):
     """Unittest for console"""
 
